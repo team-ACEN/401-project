d4b3d07bc17efc9b1dbd9ea83ac41b75a5276d10
Erik M. Dodd <emdodd27@gmail.com>
2023-02-23 09:26:08 -0800

    Working on search functionality. No errors but not returning result

--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,27 @@ import Header from '../components/Header';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
 import { useAuth } from '../contexts/Auth';
-import Navigation from '../components/Navigation';
-// import Image from 'next/image';
 // import styles from '@/styles/Home.module.css'
 
 
 export default function Home() {
-  
+
   // const { user, login } = useAuth();
-  
+
   return (
     <>
-      <Head/>
-      {/* { user ?
-        <HomePage/>
+      <Head />
+      {/* {user ?
+        <HomePage />
         :
-        <LoginForm onLogin={login}/>
+        <LoginForm onLogin={login} />
       } */}
       <Header />
       <Main />
       <Footer />
-  
+
     </>
   )
 }
+
+
